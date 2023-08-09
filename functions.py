54fe9d0b6647a9dcb6864a9dfa3bb1d2f26fb7ad
bopah <bopah91@hotmail.com>
2023-08-09 19:52:29 +0200

    added function read_gen_db
    this function reads one of the 10 datasets into a pandas dataframe, and we also sort the dataframe by pokemon-index (column 1)

--- functions.py
+++ functions.py
@@ -9,3 +9,15 @@ read_stats = pd.read_csv('pokemon dataset/pokemon_stats.csv')
 drop_duplicates_stats = read_stats.drop_duplicates(subset=['name'])
 delete_form = drop_duplicates_stats.drop('form', axis=1)
 DF_STATS = delete_form.sort_values(by=read_stats.columns[0]) # Data Frame of all pokemon stats, which is sorted according to pokemon-index
+
+# Read 1 of the 10 generation-database into a pandas dataFrame.
+def read_gen_db(generation_dropdown):
+    if generation_dropdown == 'All Time':
+        df_sort = DF_STATS
+        return df_sort
+    else:
+        gen_number = generation_dropdown[-1] # The last character is a number from 1-9
+        data_path = 'pokemon dataset/pokemon_data_gen' + gen_number + '.csv'
+        df = pd.read_csv(data_path)
+        df_sort = df.sort_values(by=df.columns[0]) # Sort generation-data based on the first column (pokemon index)
+        return df_sort

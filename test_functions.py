c0296672af4367003f3056c5f3855ef7a71ee950
bopah <bopah91@hotmail.com>
2023-08-10 09:57:33 +0200

    added test of filter_name_rows

--- test_functions.py
+++ test_functions.py
@@ -1,5 +1,6 @@
 import functions
 
+
 def test_read_gen_db():
     df = functions.read_gen_db('Generation 1')
     assert df is not None
@@ -8,6 +9,13 @@ def test_read_gen_db():
     df_all_time = functions.read_gen_db('All Time')
     assert df_all_time is not None
     assert len(df_all_time) == len(functions.DF_STATS)
+
+
+def test_filter_name_rows():
+    row_index = 0
+    filtered_df = functions.filter_name_rows(row_index)
+    assert len(filtered_df) == 1
+    assert filtered_df.iloc[0]['name'] == functions.DF_STATS.loc[row_index, 'name']
 
 
 def test_get_pokemon_stats():

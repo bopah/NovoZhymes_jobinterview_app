9bcca0e54016894c54f46a6c1bf188ae33362021
bopah <bopah91@hotmail.com>
2023-08-10 09:55:35 +0200

    made unit test for read_gen_db

--- test_functions.py
+++ test_functions.py
@@ -1,5 +1,15 @@
 import functions
 
+def test_read_gen_db():
+    df = functions.read_gen_db('Generation 1')
+    assert df is not None
+    assert len(df) > 0
+
+    df_all_time = functions.read_gen_db('All Time')
+    assert df_all_time is not None
+    assert len(df_all_time) == len(functions.DF_STATS)
+
+
 def test_get_pokemon_stats():
     # Test Strongest - We have 1 row / 1 element in list
     data_strongest = functions.get_pokemon_stats('Generation 1', 'Strongest')
